726dc8c0ee34649194537cb96301d8ae62c65b69
Pritosh2387 <pritosh2387@gmail.com>
2025-12-20 15:56:50 +0530

    Daily 20 December

--- Daily.cpp
+++ Daily.cpp
@@ -569,6 +569,17 @@ string addBinary(string a, string b){
     return ans;
 }
 //944
-int minDeletionSize(vector<string>& strs) {
-            
+int minDeletionSize(vector<string>&strs){
+    int n = strs.size();
+    int m = strs[0].size();
+    int count = 0;
+    for (int j = 0; j < m; j++) {
+        for (int i = 1; i < n; i++) {
+            if (strs[i][j] < strs[i - 1][j]) {
+                count++;
+                break; 
+            }
+        }
+    }
+    return count;
 }
