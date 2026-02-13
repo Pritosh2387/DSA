01d57d178f805bd1197b479ce57cbaa5c77dc4a5
Pritosh2387 <pritosh2387@gmail.com>
2026-02-13 22:36:01 +0530

    DP Till 30

--- DPb.cpp
+++ DPb.cpp
@@ -54,6 +54,77 @@ int unboundedknapsack_t(vector<int>&wt,vector<int>&val,int w){
     }
     return dp[n-1][w];
 }
+int longestCommonSubsequence_r(string &text1, string &text2,int i,int j){
+    if(i==-1 || j==-1)return 0;
+    if(text1[i]==text2[j]){
+        return 1 + longestCommonSubsequence_r(text1,text2,i-1,j-1);
+    }
+    return max(longestCommonSubsequence_r(text1,text2,i-1,j),longestCommonSubsequence_r(text1,text2,i,j-1));
+}
+int longestCommonSubsequence_m(string &text1, string &text2,int i,int j,vector<vector<int>>&dp){
+    if(i==-1 || j==-1)return 0;
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(text1[i]==text2[j]){
+        return dp[i][j]=1 + longestCommonSubsequence_m(text1,text2,i-1,j-1,dp);
+    }
+    return dp[i][j]=max(longestCommonSubsequence_m(text1,text2,i-1,j,dp),longestCommonSubsequence_m(text1,text2,i,j-1,dp));
+}
+int longestCommonSubsequence_t(string &text1, string &text2){
+    int n = text1.length(),m=text2.length();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+    for(int i = 0 ; i <= n ; i++){
+        dp[i][0] = 0;
+    }
+    for(int j = 0 ; j <= m ; j++){
+        dp[0][j]= 0;
+    }
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= m ; j++){
+            if(text1[i-1]==text2[j-1]){
+                dp[i][j]=1 + dp[i-1][j-1];
+            }else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+int longestCommonSubsequence(string &text1, string &text2){
+    int n = text1.length(),m=text2.length();
+    vector<int>prev(m+1, 0);
+    for(int i = 1 ; i <= n ; i++){
+        vector<int>curr(m+1, 0);
+        for(int j = 1 ; j <= m ; j++){
+            if(text1[i-1]==text2[j-1]){
+                curr[j]=1 + prev[j-1];
+            }else{
+                curr[j]=max(prev[j],curr[j-1]);
+            }
+        }
+        prev = curr;
+    }
+    return prev[m];
+}
+int longestCommonSubstring(string &text1, string &text2){
+    int ans = 0;
+    int n = text1.length(),m=text2.length();
+    vector<int>prev(m+1, 0);
+    for(int i = 1 ; i <= n ; i++){
+        vector<int>curr(m+1, 0);
+        for(int j = 1 ; j <= m ; j++){
+            if(text1[i-1]==text2[j-1]){
+                curr[j]=1 + prev[j-1];
+                ans = max(curr[j],ans);
+            }else{
+                curr[j]=0;
+            }
+        }
+        prev = curr;
+    }
+    return ans;
+}
 int main(){
     return 0;
 }
